c24d01a666e838a19c45ee9ae2cd34d3bb647642
HanifaApril <edelweis.hanifa@yahoo.com>
2013-02-12 11:17:55 +0700

    portal-siswa

--- school-studentportal-web/src/main/webapp/js/aplikasi.js
+++ school-studentportal-web/src/main/webapp/js/aplikasi.js
@@ -9,6 +9,7 @@ angular.module('belajar', ['ui', 'belajar.controller'])
             .when('/system/role', {templateUrl: 'pages/system/role.html', controller: 'RoleController'})
             .when('/system/permission', {templateUrl: 'pages/system/permission.html', controller: 'PermissionController'})
             .when('/system/menu', {templateUrl: 'pages/system/menu.html', controller: 'SystemMenuController'})
+            .when('/history/HistoryAbsensi', {templateUrl: 'pages/history/HistoryAbsensi.html'})
             .when('/about', {templateUrl: 'pages/about.html', controller: 'AboutController'})
             .otherwise({templateUrl: 'pages/404.html'});
     }])
